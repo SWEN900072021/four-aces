eede9fc197c3f0df82b82d4b183706333560e80d
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-08-27 20:44:54 +1000

    Create JDBCtest.java

---
+++ src/main/java/com/example/four_aces/JDBCtest.java
@@ -0,0 +1,37 @@
+package com.example.four_aces;
+
+import java.sql.*;
+
+public class JDBCtest {
+    private final String url = "jdbc:postgresql://localhost:5432/myDB";
+    private final String user = "postgres";
+    private final String password = "admin";
+    /**
+     * Connect to the PostgreSQL database
+     * @return a Connection object
+     */
+    public void connect() {
+        String sql = "SELECT * FROM users;";
+        PreparedStatement findStatement = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            conn = DriverManager.getConnection(url, user, password);
+            findStatement = conn.prepareStatement(sql);
+            findStatement.execute();
+            rs = findStatement.getResultSet();
+            rs.next();
+            String username = rs.getString(1);
+            System.out.println(username);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        JDBCtest app = new JDBCtest();
+        app.connect();
+    }
+}
+
