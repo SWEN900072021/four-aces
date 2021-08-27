b57414e8cb4f6721e302c2e06416c6752594e9c3
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-08-27 20:39:16 +1000

    Create testing login page

---
+++ src/main/java/com/example/four_aces/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.example.four_aces;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        System.out.println("Hello from GET method in LoginServlet");
+        String user = request.getParameter("userName");
+        String pass = request.getParameter("passWord");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h3> Hello from Get "+user+ " " +pass+ "</h3>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        System.out.println("Hello from Post method in LoginServlet");
+        String user = request.getParameter("userName");
+        String pass = request.getParameter("passWord");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h3> Hello! We are team Four Aces! </h3>");
+        writer.println("<h3> Your user name is: " + user + "</h3>");
+        writer.println("<h3> Your password is: " + pass + "</h3>");
+    }
+}
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,9 +5,14 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
+<h1><%= "Travel Reservation System" %>
 </h1>
+<h2><%= "Four Aces" %></h2>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+<form action = "login" method = "post">
+    User name: <input type = "text" name = "userName"><br/>
+    Password: <input type = "password" name = "passWord"><br/>
+    <input type = "submit" value = "Login">
+</form>
 </body>
 </html>
