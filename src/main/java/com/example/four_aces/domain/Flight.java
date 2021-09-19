5451d1faa1fd5b522085c2142c4351cddcad61fd
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-09-19 22:08:48 +1000

    delete redundant spaces

--- src/main/java/com/example/four_aces/domain/Flight.java
+++ src/main/java/com/example/four_aces/domain/Flight.java
@@ -1,11 +1,8 @@
 package main.java.com.example.four_aces.domain;
 
 public class Flight extends DomainObject {
-
     private String code;
-
     private String date;
-
     private String time;
 
     public Flight(int id, String code, String date, String time) {
