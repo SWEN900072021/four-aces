ca7e4298a782947e6a7c6b53fc46d8ca4ba6c31c
YiyuanWei <weiyiyuan52@gmail.com>
2021-09-22 16:52:12 +1000

    DBController fixed

--- src/main/java/com/example/controller/AuthenticationController.java
+++ src/main/java/com/example/controller/AuthenticationController.java
@@ -11,11 +11,6 @@ import java.util.HashMap;
 
 public class AuthenticationController {
 
-    public final static int REG_FAIL_NO_TYPE_SELECTED = -1;
-    public final static int REG_DUPLICATE_ACCOUNT = -2;
-    public final static int LOGIN_FAIL_WRONG_TYPE = -1;
-    public static final int LOGIN_FAIL_NO_USER_FOUND = -2;
-
     public User login(HashMap<String, String> params) throws TRSException, SQLException {
         User user = null;
         switch (params.remove("type")){
