908835b8f8c58832986c7d30a0eaf4a942392bf1
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-09-22 20:52:57 +1000

    delete redundant code

--- src/main/java/com/example/controller/commands/CreateFlightCommand.java
+++ src/main/java/com/example/controller/commands/CreateFlightCommand.java
@@ -9,12 +9,6 @@ import java.io.IOException;
 public class CreateFlightCommand extends FrontCommand {
     @Override
     public void processGet() throws ServletException, IOException {
-        String flightCode = request.getParameter("flightCode");
-        String flightDate= request.getParameter("flightDate");
-        String flightTime = request.getParameter("flightTime");
-        Flight flight = new Flight(null, flightCode, flightDate, flightTime);
-        UnitOfWork.getInstance().commit();
-        forward("/airline.jsp");
     }
 
     @Override
