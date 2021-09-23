efbc7d6eb3abfe6dfe5c083b801d0feb426e1d29
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-09-23 18:28:38 +1000

    only forward flights created by airline with particular id

--- src/main/java/com/example/controller/commands/GetFlightCommand.java
+++ src/main/java/com/example/controller/commands/GetFlightCommand.java
@@ -2,6 +2,7 @@ package com.example.controller.commands;
 
 import com.example.datasource.AirportDataMapper;
 import com.example.datasource.FlightDataMapper;
+import com.example.domain.Airline;
 import com.example.domain.Airport;
 import com.example.domain.Flight;
 
@@ -15,8 +16,16 @@ public class GetFlightCommand extends FrontCommand {
     public void processGet() throws ServletException, IOException {
         int airlineId = Integer.parseInt(request.getParameter("airlineId"));
         try {
-            List<Flight> flights = FlightDataMapper.getInstance().getAll();
+            List<Flight> allFlights = FlightDataMapper.getInstance().getAll();
             List<Airport> airports = AirportDataMapper.getInstance().getAll();
+            // Only forward flights created by airline with id equals airlineId
+            List<Flight> flights = new ArrayList<>();
+            for (int i = 0; i < allFlights.size(); i ++) {
+                Flight flight = allFlights.get(i);
+                if (flight.getAirlineId() == airlineId) {
+                    flights.add(flight);
+                }
+            }
             request.setAttribute("flights", flights);
             request.setAttribute("airports", airports);
         } catch (Exception e) {
