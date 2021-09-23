071bb229ff7b61b66b11ac6ac97a71d35404c837
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-09-24 00:10:29 +1000

    Create Airplane data mapper

---
+++ src/main/java/com/example/datasource/AirplaneDataMapper.java
@@ -0,0 +1,49 @@
+package com.example.datasource;
+
+import com.example.domain.Airplane;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+///////////////////// Work In Progress - Xueqi ////////////////////////
+public class AirplaneDataMapper extends AbstractDataMapper<Airplane> {
+
+    private static AirplaneDataMapper _instance;
+
+    private AirplaneDataMapper() {
+        this.table = "airplane";
+        this.fields = new String[] {"airplane_id", "airplane_type", "seats"};
+        this.pkey = "airplane_id";
+    }
+
+    public static AirplaneDataMapper getInstance(){
+        if( _instance == null ){
+            return _instance = new AirplaneDataMapper();
+        }
+        return _instance;
+    }
+
+    @Override
+    public Airplane newDomainObject(ResultSet rs) throws SQLException {
+        int airplaneId = Integer.parseInt(rs.getString("airplane_id"));
+        String airplaneType = rs.getString("airplane_type");
+        String seats = rs.getString("seats");
+        Airplane airplane = new Airplane(airplaneId, airplaneType, seats);
+        return airplane;
+    }
+
+    @Override
+    public void setPreparedStatement(PreparedStatement ps, Airplane airplane) throws Exception {
+        ps.setString(1, airplane.getType());
+        ps.setString(2, airplane.getSeatsString());
+    }
+
+    public static void main(String[] args) throws Exception {
+        AirplaneDataMapper airplaneDataMapper = AirplaneDataMapper.getInstance();
+        List<Airplane> airplanes = airplaneDataMapper.getAll();
+        System.out.println(airplanes.get(0).getSeatsString());
+    }
+}
+
--- src/main/java/com/example/domain/Airplane.java
+++ src/main/java/com/example/domain/Airplane.java
@@ -4,14 +4,25 @@ import java.util.ArrayList;
 
 public class Airplane extends DomainObject {
 
-    private ArrayList<Seat> seats;
     private String type;
+    private String seatsString;
+    private ArrayList<Seat> seats;
 
-    public Airplane() {
-        super(null);
+    public Airplane(Integer id, String type, String seatsString) {
+        super(id);
+        this.type = type;
+        this.seatsString = seatsString;
     }
 
-    public ArrayList<Seat> getAvailableSeats(){
+    public String getType() {
+        return this.type;
+    }
+
+    public String getSeatsString() {
+        return this.seatsString;
+    }
+
+    public ArrayList<Seat> getSeats() {
         return null;
     }
 }
