4bf18d9d088cd6d7dd0ec989face98144a3a6292
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-09-25 23:10:09 +1000

    connect to heroku postgres when deploy

--- src/main/java/com/example/controller/DBController.java
+++ src/main/java/com/example/controller/DBController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -9,21 +8,15 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBController {
+
     private String url;
     private String name;
     private String password;
 
+    public static final String propertyFile = "config.properties";
+
     public DBController(){
         try {
-            String propertyFile = "";
-            // Use config.properties in local development and use system.properties in deployed app
-            File f = new File("src/config.properties");
-            if (f.exists()) {
-                propertyFile = "config.properties";
-            } else {
-                propertyFile = "system.properties";
-            }
-
             Properties prop = new Properties();
             InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propertyFile);
 
@@ -36,11 +29,6 @@ public class DBController {
             this.url = prop.getProperty("url");
             this.name = prop.getProperty("name");
             this.password = prop.getProperty("password");
-
-            System.out.println(this.url);
-            System.out.println(this.name);
-            System.out.println(this.password);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -49,20 +37,16 @@ public class DBController {
     public Connection connect() throws SQLException {
         Connection connection;
         DriverManager.registerDriver(new org.postgresql.Driver());
-        connection = DriverManager.getConnection(url, name, password);
+        try {
+            connection = DriverManager.getConnection(url, name, password);
+        } catch (SQLException e) {
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            connection = DriverManager.getConnection(dbUrl);
+        }
         return connection;
     }
 
     public void close(Connection connection) throws SQLException {
         connection.close();
     }
-
-    public static void main(String[] args) throws SQLException {
-        DBController dbController = new DBController();
-        dbController.connect();
-
-        String DB_CONNECTION = System.getenv().get("url");
-        System.out.println(DB_CONNECTION);
-    }
 }
-
